55b45964930411896a4fd825ee2cf10afda6f63d
Byron Marohn <combustible@live.com>
2018-05-24 09:49:11 -0700

    Refactor paste into StructureUtils class
    Signed-off-by: Byron Marohn <combustible@live.com>

--- plugin/src/pe/epicstructures/commands/LoadStructure.java
+++ plugin/src/pe/epicstructures/commands/LoadStructure.java
@@ -1,36 +1,19 @@
 package pe.epicstructures.commands;
 
-import java.util.List;
+import com.boydti.fawe.object.schematic.Schematic;
+
+import com.sk89q.worldedit.Vector;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-
-import com.sk89q.worldedit.blocks.BaseBlock;
-import com.sk89q.worldedit.EditSession;
-import com.sk89q.worldedit.entity.Entity;
-import com.sk89q.worldedit.entity.metadata.EntityType;
-import com.sk89q.worldedit.extent.clipboard.Clipboard;
-import com.sk89q.worldedit.function.EntityFunction;
-import com.sk89q.worldedit.function.operation.Operations;
-import com.sk89q.worldedit.function.RegionFunction;
-import com.sk89q.worldedit.function.visitor.EntityVisitor;
-import com.sk89q.worldedit.function.visitor.RegionVisitor;
-import com.sk89q.worldedit.MutableBlockVector2D;
-import com.sk89q.worldedit.regions.CuboidRegion;
-import com.sk89q.worldedit.regions.Region;
-import com.sk89q.worldedit.Vector;
-import com.sk89q.worldedit.WorldEditException;
-
-import com.boydti.fawe.object.HasFaweQueue;
-import com.boydti.fawe.object.schematic.Schematic;
-import com.boydti.fawe.util.EditSessionBuilder;
+import org.bukkit.Location;
 
 import pe.epicstructures.Plugin;
 import pe.epicstructures.utils.CommandUtils;
 import pe.epicstructures.utils.MessagingUtils;
+import pe.epicstructures.utils.StructureUtils;
 
 public class LoadStructure implements CommandExecutor {
 	Plugin mPlugin;
@@ -79,105 +62,10 @@ public class LoadStructure implements CommandExecutor {
 			return false;
 		}
 
-		// TODO: Whatever is going on here... entities are broken IF:
-		// fastmode = true (regardless of combine stages setting)
-		// fastmode = false AND combineStages = true
-		EditSession copyWorld = new EditSessionBuilder(mWorld.getName()).autoQueue(true).fastmode(false).combineStages(false).build();
-
-		_paste(schem.getClipboard(), copyWorld, loadPos);
-
-		copyWorld.flushQueue();
+		StructureUtils.paste(schem.getClipboard(), mWorld, loadPos);
 
 		sender.sendMessage("Loaded structure '" + arg3[0] + "' at " + loadPos);
 
 		return true;
-	}
-
-	// Custom paste function copied and modified from
-	// FastAsyncWorldedit/core/src/main/java/com/boydti/fawe/object/schematic/Schematic.java
-	//
-	// Ignores structure blocks, leaving the original block in place
-	public void _paste(Clipboard clipboard, EditSession extent, Vector to) {
-		Region sourceRegion = clipboard.getRegion().clone();
-		Region destRegion = new CuboidRegion(to, new Vector(to).add(sourceRegion.getMaximumPoint()).subtract(sourceRegion.getMinimumPoint()));
-		final Vector origin = clipboard.getOrigin();
-
-		// ******************************** Blocks *************************************** //
-		final int relx = to.getBlockX() - origin.getBlockX();
-		final int rely = to.getBlockY() - origin.getBlockY();
-		final int relz = to.getBlockZ() - origin.getBlockZ();
-		RegionVisitor regionVisitor = new RegionVisitor(sourceRegion, new RegionFunction() {
-			MutableBlockVector2D mpos2d_2 = new MutableBlockVector2D();
-			MutableBlockVector2D mpos2d = new MutableBlockVector2D();
-			{
-				mpos2d.setComponents(Integer.MIN_VALUE, Integer.MIN_VALUE);
-			}
-			@Override
-			public boolean apply(Vector mutable) throws WorldEditException {
-				BaseBlock block = clipboard.getBlock(mutable);
-				int xx = mutable.getBlockX() + relx;
-				int yy = mutable.getBlockY() + rely;
-				int zz = mutable.getBlockZ() + relz;
-
-				// Don't paste in structure void
-				if (block.getId() == 217) {
-					return false;
-				}
-
-				extent.setBlock(xx, yy, zz, block);
-				return false;
-			}
-		}, extent);
-
-		Operations.completeBlindly(regionVisitor);
-
-		// ******************************** Entities *************************************** //
-		/*
-		 * Make an iterable function that removes all the things that should
-		 * be removed when a POI resets
-		 */
-		EntityFunction removeFunc = new EntityFunction() {
-			@Override
-			public boolean apply(Entity entity) throws WorldEditException {
-				EntityType type = entity.getFacet(EntityType.class);
-				Vector loc = entity.getLocation().toVector();
-
-				// Don't remove entities in parts of the POI that don't reset
-				if (clipboard.getBlock(loc.subtract(to).add(origin)).getId() == 217) {
-					return false;
-				}
-
-				if (type == null ||
-					type.isPlayerDerived() ||
-					type.isAnimal() ||
-					type.isTamed() ||
-					type.isGolem() ||
-					type.isNPC() ||
-					type.isArmorStand()) {
-					return false;
-				}
-
-				if (type.isLiving() ||
-					//type.isItem() ||
-					type.isProjectile() ||
-					type.isFallingBlock() ||
-					type.isBoat() ||
-					type.isTNT() ||
-					type.isExperienceOrb()) {
-					entity.remove();
-					return true;
-				}
-
-				return false;
-			}
-		};
-
-		// Get the currently loaded entities in the destination region
-		List <? extends Entity > entities = extent.getEntities(destRegion);
-
-		// Visit those entities and when visited, remove them
-		EntityVisitor EntityVisitor = new EntityVisitor(entities.iterator(), removeFunc);
-
-		Operations.completeBlindly(EntityVisitor);
 	}
 }
