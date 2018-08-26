6f25b35233166af3b3a48d4bc5ca627090714355
Byron Marohn <combustible@live.com>
2018-08-25 18:50:11 -0700

    Minor updates to maven compile
    Signed-off-by: Byron Marohn <combustible@live.com>

--- plugin/compile.sh
+++ plugin/compile.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ant pmd build jar
+# Update version number
+perl -p -i -e "s|<version>dev</version>|<version>$(git describe --tags --always --dirty)</version>|g" pom.xml
+
+mvn clean install
 retcode=$?
 if [[ $retcode -ne 0 ]]; then
 	exit $retcode
