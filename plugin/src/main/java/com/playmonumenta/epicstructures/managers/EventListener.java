1eb6247f5173ae6a9ecdba953089da3e66864149
Byron Marohn <combustible@live.com>
2019-03-04 10:54:38 -0800

    Blocks broken via explosion count towards spawner break trigger
    Signed-off-by: Byron Marohn <combustible@live.com>

--- plugin/src/main/java/com/playmonumenta/epicstructures/managers/EventListener.java
+++ plugin/src/main/java/com/playmonumenta/epicstructures/managers/EventListener.java
@@ -1,17 +1,19 @@
 package com.playmonumenta.epicstructures.managers;
 
-import com.playmonumenta.epicstructures.Plugin;
-
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.Material;
+
+import com.playmonumenta.epicstructures.Plugin;
 
 public class EventListener implements Listener {
 	Plugin mPlugin = null;
@@ -20,7 +22,7 @@ public class EventListener implements Listener {
 		mPlugin = plugin;
 	}
 
-	@EventHandler(priority = EventPriority.HIGHEST)
+	@EventHandler(priority = EventPriority.MONITOR)
 	public void PlayerInteractEvent(PlayerInteractEvent event) {
 		Action action = event.getAction();
 		Player player = event.getPlayer();
@@ -36,12 +38,34 @@ public class EventListener implements Listener {
 		}
 	}
 
-	@EventHandler(priority = EventPriority.LOWEST)
+	@EventHandler(priority = EventPriority.MONITOR)
 	public void BlockBreakEvent(BlockBreakEvent event) {
 		if (!event.isCancelled()) {
 			Block block = event.getBlock();
 			if (block.getType() == Material.SPAWNER) {
 				mPlugin.mRespawnManager.spawnerBreakEvent(block.getLocation());
+			}
+		}
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void BlockExplodeEvent(BlockExplodeEvent event) {
+		if (!event.isCancelled()) {
+			for (Block block : event.blockList()) {
+				if (block.getType() == Material.SPAWNER) {
+					mPlugin.mRespawnManager.spawnerBreakEvent(block.getLocation());
+				}
+			}
+		}
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void EntityExplodeEvent(EntityExplodeEvent event) {
+		if (!event.isCancelled()) {
+			for (Block block : event.blockList()) {
+				if (block.getType() == Material.SPAWNER) {
+					mPlugin.mRespawnManager.spawnerBreakEvent(block.getLocation());
+				}
 			}
 		}
 	}
