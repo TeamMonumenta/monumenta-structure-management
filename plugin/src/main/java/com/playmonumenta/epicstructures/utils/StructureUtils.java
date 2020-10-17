3d22f9f3d5aaeef6b1c7ef15bfc3ac4df8a831c8
Byron Marohn <combustible@live.com>
2020-10-17 00:29:07 +0000

    Significantly improve performance of structure pasting
    Signed-off-by: Byron Marohn <combustible@live.com>

--- plugin/src/main/java/com/playmonumenta/epicstructures/utils/StructureUtils.java
+++ plugin/src/main/java/com/playmonumenta/epicstructures/utils/StructureUtils.java
@@ -1,6 +1,5 @@
 package com.playmonumenta.epicstructures.utils;
 
-import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.Set;
@@ -14,18 +13,17 @@ import com.sk89q.worldedit.EditSession;
 import com.sk89q.worldedit.bukkit.BukkitWorld;
 import com.sk89q.worldedit.extent.clipboard.BlockArrayClipboard;
 import com.sk89q.worldedit.function.RegionFunction;
-import com.sk89q.worldedit.function.mask.SingleBlockTypeMask;
 import com.sk89q.worldedit.function.operation.ForwardExtentCopy;
 import com.sk89q.worldedit.function.operation.Operations;
 import com.sk89q.worldedit.math.BlockVector2;
 import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.regions.CuboidRegion;
 import com.sk89q.worldedit.regions.Region;
 import com.sk89q.worldedit.world.block.BlockType;
 import com.sk89q.worldedit.world.block.BlockTypes;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
@@ -34,10 +32,12 @@ import org.bukkit.block.BrewingStand;
 import org.bukkit.block.Chest;
 import org.bukkit.block.CreatureSpawner;
 import org.bukkit.block.Furnace;
+import org.bukkit.block.ShulkerBox;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Tameable;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -45,117 +45,67 @@ import org.bukkit.util.BoundingBox;
 import org.bukkit.util.Vector;
 
 public class StructureUtils {
-	private static final boolean LIGHT_CLEANER_ENABLED = Bukkit.getPluginManager().isPluginEnabled("LightCleaner");
-	private static final BlockType shulkerBoxes[] = {
-		BlockTypes.SHULKER_BOX,
-		BlockTypes.WHITE_SHULKER_BOX,
-		BlockTypes.ORANGE_SHULKER_BOX,
-		BlockTypes.MAGENTA_SHULKER_BOX,
-		BlockTypes.LIGHT_BLUE_SHULKER_BOX,
-		BlockTypes.YELLOW_SHULKER_BOX,
-		BlockTypes.LIME_SHULKER_BOX,
-		BlockTypes.PINK_SHULKER_BOX,
-		BlockTypes.GRAY_SHULKER_BOX,
-		BlockTypes.LIGHT_GRAY_SHULKER_BOX,
-		BlockTypes.CYAN_SHULKER_BOX,
-		BlockTypes.PURPLE_SHULKER_BOX,
-		BlockTypes.BLUE_SHULKER_BOX,
-		BlockTypes.BROWN_SHULKER_BOX,
-		BlockTypes.GREEN_SHULKER_BOX,
-		BlockTypes.RED_SHULKER_BOX,
-		BlockTypes.BLACK_SHULKER_BOX
-	};
-	private static final Set<BlockType> shulkerSet = new HashSet<>(Arrays.asList(shulkerBoxes));
-
 	// Ignores structure void, leaving the original block in place
-	public static void paste(Plugin plugin, BlockArrayClipboard clipboard, World world, BlockVector3 to, boolean includeEntities) {
+	public static void paste(final Plugin plugin, final BlockArrayClipboard clipboard, final World world, final BlockVector3 to, final boolean includeEntities) {
 
-		long startTime = System.currentTimeMillis(); // <-- START
+		final long initialTime = System.currentTimeMillis(); // <-- START
 
-		EditSession extent = new EditSessionBuilder(new BukkitWorld(world))
-			.autoQueue(true)
-			.fastmode(true)
-			.combineStages(true)
-			.changeSetNull()
-			.checkMemory(false)
-			.allowedRegionsEverywhere()
-			.limitUnlimited()
-			.build();
-
-		Region sourceRegion = clipboard.getRegion();
+		final Region sourceRegion = clipboard.getRegion();
 		final BlockVector3 size = sourceRegion.getMaximumPoint().subtract(sourceRegion.getMinimumPoint());
 		final Vector pos1 = new Vector((double)to.getX(), (double)to.getY(), (double)to.getZ());
 		final Vector pos2 = pos1.clone().add(new Vector(size.getX() + 1, size.getY() + 1, size.getZ() + 1));
 		final BoundingBox box = new BoundingBox(pos1.getX(), pos1.getY(), pos1.getZ(), pos2.getX(), pos2.getY(), pos2.getZ());
 
-		/* Filter blocks from being pasted that would land on top of A */
-		final int relx = to.getBlockX();
-		final int rely = to.getBlockY();
-		final int relz = to.getBlockZ();
-		RegionFunction filterFunction = position -> {
-				int x = position.getBlockX();
-				int y = position.getBlockY();
-				int z = position.getBlockZ();
-				BlockType oldBlockType = null;
-
-				/*
-				 * Attempt to get the existing block type. If this errors for some reason, just proceed to overwrite
-				 * the block anyway if it's not structure void
-				 */
-				try {
-					oldBlockType = extent.getBlockType(x + relx, y + rely, z + relz);
-				} catch (Exception e) {
-					plugin.getLogger().warning("Caught error in FAWE extent.getBlock(). This should be reported to FAWE");
-					e.printStackTrace();
-				}
-				BlockType newBlockType = clipboard.getBlock(position).getBlockType();
-				if (oldBlockType != null && oldBlockType.equals(BlockTypes.CHEST)) {
-					Chest chest = (Chest) world.getBlockAt(x + relx, y + rely, z + relz).getState();
-					if (chest.getCustomName() != null && chest.getCustomName().endsWith("'s Grave")) {
-						// Check if the grave has items inside. If it is empty, it can be overwritten.
-						for (ItemStack item : chest.getInventory()) {
-							if (item != null) {
-								return false;
-							}
-						}
-					}
-				}
-				if (oldBlockType != null && shulkerSet.contains(oldBlockType)) {
-					// Don't allow Shulker Boxes to be overwritten
-					return false;
-				}
-				if (newBlockType == null || !newBlockType.equals(BlockTypes.STRUCTURE_VOID)) {
-					return true;
-				}
-				return false;
-			};
-
-
-		Region shiftedRegion = clipboard.getRegion().clone();
+		final Region shiftedRegion = clipboard.getRegion().clone();
 		shiftedRegion.shift(to);
 
-		Set<BlockVector2> chunks = shiftedRegion.getChunks();
-		AtomicInteger numRemaining = new AtomicInteger(chunks.size());
+		final Set<BlockVector2> chunks = shiftedRegion.getChunks();
+		final AtomicInteger numRemaining = new AtomicInteger(chunks.size());
+
+		/* Set of positions (relative to the clipboard / origin) that should not be overwritten when pasting */
+		final Set<Long> noLoadPositions = new HashSet<>();
 
 		/* This chunk consumer removes entities and sets spawners/brewstands/furnaces to air */
-		Consumer<Chunk> chunkConsumer = (Chunk chunk) -> {
+		final Consumer<Chunk> chunkConsumer = (final Chunk chunk) -> {
 			numRemaining.decrementAndGet();
-			for (BlockState state : chunk.getTileEntities()) {
-				if (state instanceof CreatureSpawner || state instanceof BrewingStand || state instanceof Furnace) {
-					org.bukkit.Location loc = state.getLocation();
-					BlockVector3 relPos = BlockVector3.at(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()).subtract(to);
+			for (final BlockState state : chunk.getTileEntities(true)) {
+				if (state instanceof CreatureSpawner || state instanceof BrewingStand || state instanceof Furnace || state instanceof Chest || state instanceof ShulkerBox) {
+					final org.bukkit.Location loc = state.getLocation();
+					final BlockVector3 relPos = BlockVector3.at(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()).subtract(to);
 					if (box.contains(loc.toVector()) && !clipboard.getBlock(relPos).getBlockType().equals(BlockTypes.STRUCTURE_VOID)) {
-						Block block = state.getBlock();
-						block.setType(Material.DIRT);
-						block.setBlockData(Material.DIRT.createBlockData());
+						if (state instanceof CreatureSpawner || state instanceof BrewingStand || state instanceof Furnace) {
+							// TODO: Work around a bug in FAWE that corrupts these blocks if they're not removed first
+							final Block block = state.getBlock();
+
+							/* Set block to air and then dirt... which works around somehow the tile entity data being left behind */
+							if (state instanceof BrewingStand || state instanceof Furnace) {
+								Inventory inv;
+								if (state instanceof BrewingStand) {
+									inv = ((BrewingStand)state).getInventory();
+								} else {
+									inv = ((Furnace)state).getInventory();
+								}
+								for (int i = 0; i < inv.getSize(); i++) {
+									inv.setItem(i, new ItemStack(Material.AIR));
+								}
+							}
+							block.setType(Material.AIR);
+							block.setType(Material.DIRT);
+						} else if (state instanceof ShulkerBox || ((state instanceof Chest) && isNonemptyGrave((Chest)state))) {
+							/* Never overwrite shulker boxes or player graves with contents */
+							final int relx = state.getX() - to.getX();
+							final int rely = state.getY() - to.getY();
+							final int relz = state.getZ() - to.getZ();
+							noLoadPositions.add(compressToLong(relx, rely, relz));
+						}
 					}
 				}
 			}
 
 			if (includeEntities) {
-				for (Entity entity : chunk.getEntities()) {
+				for (final Entity entity : chunk.getEntities()) {
 					if (box.contains(entity.getLocation().toVector()) && entityShouldBeRemoved(entity)) {
-						Vector relPos = entity.getLocation().toVector().subtract(pos1);
+						final Vector relPos = entity.getLocation().toVector().subtract(pos1);
 						if (!clipboard.getBlock(BlockVector3.at(relPos.getBlockX(), relPos.getBlockY(), relPos.getBlockZ())).getBlockType().equals(BlockTypes.STRUCTURE_VOID)) {
 							entity.remove();
 						}
@@ -165,7 +115,7 @@ public class StructureUtils {
 		};
 
 		/* Load all the chunks in the region and run the chunk consumer */
-		for (BlockVector2 chunkCoords : shiftedRegion.getChunks()) {
+		for (final BlockVector2 chunkCoords : shiftedRegion.getChunks()) {
 			world.getChunkAtAsync(chunkCoords.getX(), chunkCoords.getZ(), chunkConsumer);
 		}
 
@@ -182,37 +132,70 @@ public class StructureUtils {
 				if (numRemaining.get() == 0) {
 					this.cancel();
 
-					new BukkitRunnable() {
-						@Override
-						public void run() {
-							ForwardExtentCopy copy = new ForwardExtentCopy(clipboard, clipboard.getRegion(), clipboard.getOrigin(), extent, to);
+					/* Actually load the structure synchronously now that all the chunks have been processed for entities / blocks that shouldn't be replaced */
+					Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+						plugin.getLogger().info("Initial processing took " + Long.toString(System.currentTimeMillis() - initialTime) + " milliseconds (mostly async)"); // STOP -->
+
+						final long pasteTime = System.currentTimeMillis(); // <-- START
+						try (EditSession extent = new EditSessionBuilder(new BukkitWorld(world))
+							.autoQueue(true)
+							.fastmode(true)
+							.combineStages(true)
+							.changeSetNull()
+							.checkMemory(false)
+							.allowedRegionsEverywhere()
+							.limitUnlimited()
+							.build()) {
+
+							/*
+							 * Filter function to skip some blocks from overwriting what exists in the world
+							 * If this function returns true, this location will be overwritten
+							 */
+							final RegionFunction filterFunction = position -> {
+								final BlockType newBlockType = clipboard.getBlock(position).getBlockType();
+
+								if (newBlockType == null || !newBlockType.equals(BlockTypes.STRUCTURE_VOID)) {
+									// This position is not in structure void in the clipboard
+									if (!noLoadPositions.contains(compressToLong(position.getBlockX(), position.getBlockY(), position.getBlockZ()))) {
+										// This position is not in the list of blocks that should not be overwritten
+										return true;
+									}
+								}
+
+								// Don't overwrite by default
+								return false;
+							};
+
+
+							final ForwardExtentCopy copy = new ForwardExtentCopy(clipboard, clipboard.getRegion(), clipboard.getOrigin(), extent, to);
 							copy.setCopyingBiomes(false);
 							copy.setFilterFunction(filterFunction);
 							copy.setCopyingEntities(includeEntities);
-
-							plugin.getLogger().info("Preparing paste took " + Long.toString(System.currentTimeMillis() - startTime) + " milliseconds"); // STOP -->
-
-							long startTime = System.currentTimeMillis(); // <-- START
 							Operations.completeBlindly(copy);
-							plugin.getLogger().info("completeBlindly took " + Long.toString(System.currentTimeMillis() - startTime) + " milliseconds"); // STOP -->
-
-							startTime = System.currentTimeMillis(); // <-- START
-							extent.flushQueue();
-							plugin.getLogger().info("flushQueue took " + Long.toString(System.currentTimeMillis() - startTime) + " milliseconds"); // STOP -->
-
-							/*
-							 * Fix lighting after the structure loads (if plugin present)
-							 */
-							new BukkitRunnable() {
-								@Override
-								public void run() {
-									long startTime = System.currentTimeMillis(); // <-- START
-									scheduleLighting(world, to, size);
-									plugin.getLogger().info("scheduleLighting took " + Long.toString(System.currentTimeMillis() - startTime) + " milliseconds"); // STOP -->
-								}
-							}.runTaskLater(plugin, 40);
 						}
-					}.runTaskLater(plugin, 2);
+						plugin.getLogger().info("Loading structure took " + Long.toString(System.currentTimeMillis() - pasteTime) + " milliseconds (async)"); // STOP -->
+
+						/* Schedule light cleaning on the main thread so it can safely check plugin enabled status */
+						Bukkit.getScheduler().runTask(plugin, () -> {
+							if (!Bukkit.getPluginManager().isPluginEnabled("LightCleaner")) {
+								return;
+							}
+
+							Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+								final long lightTime = System.currentTimeMillis(); // <-- START
+
+								/* Relight an area 16 blocks bigger than the respawned area */
+								final Set<BlockVector2> lightingChunks = new CuboidRegion(to.subtract(16, 16, 16), to.add(size).add(16, 16, 16)).getChunks();
+								final LongHashSet lightCleanerChunks = new LongHashSet(lightingChunks.size());
+								for (final BlockVector2 chunk : lightingChunks) {
+									lightCleanerChunks.add(chunk.getX(), chunk.getZ());
+								}
+								LightingService.schedule(world, lightCleanerChunks);
+
+								plugin.getLogger().info("scheduleLighting took " + Long.toString(System.currentTimeMillis() - lightTime) + " milliseconds (async)"); // STOP -->
+							});
+						});
+					});
 				}
 			}
 		}.runTaskTimer(plugin, 0, 1);
@@ -232,7 +215,7 @@ public class StructureUtils {
 		EntityType.CHICKEN
 	);
 
-	private static boolean entityShouldBeRemoved(Entity entity) {
+	private static boolean entityShouldBeRemoved(final Entity entity) {
 		/* Keep some entity types always */
 		if (keptEntities.contains(entity.getType())) {
 			return false;
@@ -240,17 +223,17 @@ public class StructureUtils {
 
 		/* Keep armor stands that have a name, are markers, or have tags */
 		if (entity instanceof ArmorStand) {
-			ArmorStand stand = (ArmorStand)entity;
+			final ArmorStand stand = (ArmorStand)entity;
 			if ((stand.getCustomName() != null && !stand.getCustomName().isEmpty())
-				|| stand.isMarker()
-				|| (stand.getScoreboardTags() != null && !stand.getScoreboardTags().isEmpty())) {
+			    || stand.isMarker()
+			    || (stand.getScoreboardTags() != null && !stand.getScoreboardTags().isEmpty())) {
 				return false;
 			}
 		}
 
 		/* Keep tameable critters that have an owner */
 		if (entity instanceof Tameable) {
-			Tameable critter = (Tameable)entity;
+			final Tameable critter = (Tameable)entity;
 			if (critter.getOwner() != null) {
 				return false;
 			}
@@ -260,23 +243,28 @@ public class StructureUtils {
 		return true;
 	}
 
-	public static void scheduleLighting(World world, BlockVector3 to, BlockVector3 size) {
-		if (!LIGHT_CLEANER_ENABLED) {
-			return;
-		}
+	public static void scheduleLighting(final World world, final BlockVector3 to, final BlockVector3 size) {
+	}
 
-		// Pad one chunk on all sides
-		final int originX = (to.getBlockX() / 16) - 1;
-		final int originZ = (to.getBlockZ() / 16) - 1;
-		final int sizeX = (((size.getBlockX() - 1) / 16) + 3);
-		final int sizeZ = (((size.getBlockZ() - 1) / 16) + 3);
+	private static Long compressToLong(final int x, final int y, final int z) {
+		return Long.valueOf(
+		           (((long)(x & ((1 << 21) - 1))) << 42) |
+		           (((long)(y & ((1 << 21) - 1))) << 21) |
+		           ((long)(z & ((1 << 21) - 1)))
+		       );
+	}
 
-		LongHashSet chunks = new LongHashSet(sizeX * sizeZ);
-		for (int x = 0; x < sizeX; x++) {
-			for (int z = 0; z < sizeZ; z++) {
-				chunks.add(originX + x, originZ + z);
+	private static boolean isNonemptyGrave(final Chest chest) {
+		if (chest.getCustomName() != null && chest.getCustomName().endsWith("'s Grave")) {
+			// Check if the grave has items inside. If it is empty, it can be overwritten.
+			for (final ItemStack item : chest.getInventory()) {
+				if (item != null) {
+					// This is a grave with items left - don't overwrite it
+					return true;
+				}
 			}
 		}
-		LightingService.schedule(world, chunks);
+		return false;
 	}
+
 }
