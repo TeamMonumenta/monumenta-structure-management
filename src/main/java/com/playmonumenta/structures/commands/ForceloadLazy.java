e52ba939921d400054052a56791c130a468fcbd7
Byron Marohn <Combustible@live.com>
2023-01-10 20:27:49 +0000

    Add optional function callback to /forceload addlazy
    Signed-off-by: Byron Marohn <Combustible@live.com>

--- src/main/java/com/playmonumenta/structures/commands/ForceloadLazy.java
+++ src/main/java/com/playmonumenta/structures/commands/ForceloadLazy.java
@@ -3,11 +3,13 @@ package com.playmonumenta.structures.commands;
 import com.sk89q.worldedit.math.BlockVector2;
 import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.regions.CuboidRegion;
-import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.CommandPermission;
+import dev.jorel.commandapi.CommandTree;
+import dev.jorel.commandapi.arguments.FunctionArgument;
+import dev.jorel.commandapi.arguments.LiteralArgument;
 import dev.jorel.commandapi.arguments.Location2DArgument;
 import dev.jorel.commandapi.arguments.LocationType;
-import dev.jorel.commandapi.arguments.MultiLiteralArgument;
+import dev.jorel.commandapi.wrappers.FunctionWrapper;
 import dev.jorel.commandapi.wrappers.Location2D;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -15,33 +17,35 @@ import java.util.function.Consumer;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Chunk;
 import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.Nullable;
 
 public class ForceloadLazy {
 	public static void register() {
-		final CommandPermission perms = CommandPermission.fromString("monumenta.structures.forceloadlazy");
+		new CommandTree("forceload")
+			.then(new LiteralArgument("addlazy")
+				.withPermission(CommandPermission.fromString("monumenta.structures.forceloadlazy"))
 
-		/* First one of these includes coordinate arguments */
-		new CommandAPICommand("forceload")
-			.withPermission(perms)
-			.withArguments(new MultiLiteralArgument("addlazy"))
-			.withArguments(new Location2DArgument("from", LocationType.BLOCK_POSITION))
-			.executes((sender, args) -> {
-				load(sender, (Location2D)args[1], (Location2D)args[1]); // Intentionally both the same argument
-			})
-			.register();
+				// forceload addlazy from
+				.then(new Location2DArgument("from", LocationType.BLOCK_POSITION)
+					.executes((sender, args) -> {
+						load(sender, (Location2D) args[0], (Location2D) args[0], null); // Intentionally both the same argument
+					})
 
-		new CommandAPICommand("forceload")
-			.withPermission(perms)
-			.withArguments(new MultiLiteralArgument("addlazy"))
-			.withArguments(new Location2DArgument("from", LocationType.BLOCK_POSITION))
-			.withArguments(new Location2DArgument("to", LocationType.BLOCK_POSITION))
-			.executes((sender, args) -> {
-				load(sender, (Location2D)args[1], (Location2D)args[2]);
-			})
+					// forceload addlazy from to
+					.then(new Location2DArgument("to", LocationType.BLOCK_POSITION)
+						.executes((sender, args) -> {
+							load(sender, (Location2D) args[0], (Location2D) args[1], null);
+						})
+
+						// forceload addlazy from to callback
+						.then(new FunctionArgument("callback")
+							.executes((sender, args) -> {
+								load(sender, (Location2D) args[0], (Location2D) args[1], (FunctionWrapper[]) args[2]);
+							})))))
 			.register();
 	}
 
-	private static void load(CommandSender sender, Location2D from, Location2D to) {
+	private static void load(CommandSender sender, Location2D from, Location2D to, @Nullable FunctionWrapper[] callback) {
 		final Set<BlockVector2> chunks = new CuboidRegion(BlockVector3.at(from.getBlockX(), 0, from.getBlockZ()),
 				BlockVector3.at(to.getBlockX(), 255, to.getBlockZ())).getChunks();
 
@@ -62,6 +66,11 @@ public class ForceloadLazy {
 			chunk.setForceLoaded(true);
 			int remaining = numRemaining.decrementAndGet();
 			if (remaining == 0) {
+				if (callback != null) {
+					for (FunctionWrapper func : callback) {
+						func.run(sender);
+					}
+				}
 				sender.sendMessage(sendMessage);
 			}
 		};
