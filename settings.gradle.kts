eddba969ae35e3fd67fdc4bf3e3d68d07dfa26e1
Nobody314159 <84757662+Nobody314159@users.noreply.github.com>
2024-05-09 15:26:56 -0700

    Update Deployment (#57)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,9 @@
 rootProject.name = "structures"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+	maven("https://papermc.io/repo/repository/maven-public/")
+	maven("https://maven.floweytf.com/releases")
+    }
+}
